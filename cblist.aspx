373b75974501ff247998f577796ebf73bac8c189
hemamku <hemamku17@gmail.com>
2020-09-03 12:15:41 +0530

    Add files via upload

---
+++ cblist.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cblist.aspx.cs" Inherits="Checkboxlist.cblist" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:CheckBox ID="chkAll" Text="Select All" runat="server" OnCheckedChanged = "Check_UnCheckAll" AutoPostBack = "true" />
+<asp:CheckBoxList ID="chkFruits" runat="server" OnSelectedIndexChanged = "CheckBoxList_Checked_Unchecked" AutoPostBack = "true">
+    <asp:ListItem Text="Mango" />
+    <asp:ListItem Text="Apple" />
+    <asp:ListItem Text="Banana" />
+    <asp:ListItem Text="Pineapple" />
+    <asp:ListItem Text="Guava" />
+    <asp:ListItem Text="Grapes" />
+    <asp:ListItem Text="Papaya" />
+</asp:CheckBoxList>
+            </div>
+    </form>
+</body>
+</html>
---
+++ cblist.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Checkboxlist
+{
+    public partial class cblist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void CheckBoxList_Checked_Unchecked(object sender, EventArgs e)
+        {
+            bool isAllChecked = true;
+            foreach (ListItem item in chkFruits.Items)
+            {
+                if (!item.Selected)
+                {
+                    isAllChecked = false;
+                    break;
+                }
+            }
+
+            chkAll.Checked = isAllChecked;
+        }
+
+        protected void Check_UnCheckAll(object sender, EventArgs e)
+        {
+            foreach (ListItem item in chkFruits.Items)
+            {
+                item.Selected = chkAll.Checked;
+               }
+        }
+    }
+}
